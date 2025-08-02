a17913c28a4a3bce5ddf3f04845e5f52076db050
Your Name <you@example.com>
2025-08-02 15:12:58 +0200

    ajout objet calculator avec 4 fonctions

--- testing-pactice.test.js
+++ testing-pactice.test.js
@@ -1,15 +1,32 @@
 // testing-practice.test.js
 
-const {capitalize, reverseString} = require("./testing-practice");
+const { capitalize, reverseString, calculator } = require("./testing-practice");
 
 test("should capitalize the first letter of the word", () => {
     const input = "bonjour";
-    const output = "Bonjour"; 
+    const output = "Bonjour";
     expect(capitalize(input)).toBe(output);
 });
 
 test("should reverse a string", () => {
     const input = "bonjour";
     const output = "ruojnob";
-    expect(reverseString(input)).toBe(output); 
+    expect(reverseString(input)).toBe(output);
 });
+
+test("should add two numbers", () => {
+    expect(calculator.add(3, 5)).toBe(8);
+})
+
+test("should subtract two numbers", () => {
+    expect(calculator.subtract(10, 5)).toBe(5);
+});
+
+test("should multiply two numbers", () => {
+    expect(calculator.multiply(5, 10)).toBe(50);
+});
+
+test("should divide two numbers", () => {
+    expect(calculator.divide(10, 5)).toBe(2);
+});
+
--- testing-practice.js
+++ testing-practice.js
@@ -1,7 +1,7 @@
 // testing-practice.js
 
 function capitalize(string) {
-return string[0].toUpperCase() + string.slice(1); 
+    return string[0].toUpperCase() + string.slice(1);
 }
 
 
@@ -9,10 +9,25 @@ function reverseString(string) {
     // split("") convertit une chaîne en un tableau de caractères
     // reverse("") inverse ordre éléments du tableau
     // join("" recombine le tableau en une chaîne de caractères)
-    return string.split("").reverse().join(""); 
+    return string.split("").reverse().join("");
+}
+const calculator = {
+    add(a, b) {
+        return a + b;
+    },
+    subtract(a, b) {
+        return a - b;
+    },
+    multiply(a, b) {
+        return a * b;
+    },
+    divide(a, b) {
+        return a / b;
+    }
 }
 
 module.exports = {
     capitalize,
-    reverseString
+    reverseString,
+    calculator
 };
