9b96e75984cea4a3d3c509eafdaec22789b3bddb
Your Name <you@example.com>
2025-08-02 14:58:06 +0200

    ajout fonction reverseString

--- testing-pactice.test.js
+++ testing-pactice.test.js
@@ -1,9 +1,15 @@
 // testing-practice.test.js
 
-const capitalize = require("./testing-practice");
+const {capitalize, reverseString} = require("./testing-practice");
 
 test("should capitalize the first letter of the word", () => {
     const input = "bonjour";
     const output = "Bonjour"; 
     expect(capitalize(input)).toBe(output);
+});
+
+test("should reverse a string", () => {
+    const input = "bonjour";
+    const output = "ruojnob";
+    expect(reverseString(input)).toBe(output); 
 });
--- testing-practice.js
+++ testing-practice.js
@@ -4,4 +4,15 @@ function capitalize(string) {
 return string[0].toUpperCase() + string.slice(1); 
 }
 
-module.exports = capitalize; 
+
+function reverseString(string) {
+    // split("") convertit une chaîne en un tableau de caractères
+    // reverse("") inverse ordre éléments du tableau
+    // join("" recombine le tableau en une chaîne de caractères)
+    return string.split("").reverse().join(""); 
+}
+
+module.exports = {
+    capitalize,
+    reverseString
+};
